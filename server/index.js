6058ecacda36189ca6589d3444d3b64eb7220637
evondev <trananhtuan400@gmail.com>
2022-06-05 15:04:06 +0700

    Setup Node Server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3009;
+
+server.use(middleware);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
