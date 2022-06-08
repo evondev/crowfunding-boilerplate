323bac271280b4f8836104d8362ed6d1e4a9d09e
evondev <trananhtuan400@gmail.com>
2022-06-08 12:05:24 +0700

    Setup source code

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
+require("dotenv").config();
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults();
-
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const PORT = process.env.PORT || 3009;
+const app = express();
+app.use(express.json());
+app.use("/api", jsonServer.defaults(), jsonServer.router("db.json"));
 
-server.use(middleware);
-server.use(router);
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+const users = [
+  {
+    username: "evondev",
+    fullname: "Tran Anh Tuan",
+  },
+  {
+    username: "thangtkt",
+    fullname: "Phan Tan Thang",
+  },
+];
+
+app.get("/users", authenticateToken, (req, res) => {
+  res.json(users.filter((user) => user.username === req.user.name));
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
